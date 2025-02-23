ce30ed7dc8f2046d7c3c17798653f44ed7f9f279
BriTheCode <https://github.com/BriTheCode>
2025-02-23 23:13:06 +0800

    changing to the header

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include("database.php");
     <title>Dela Cruz</title>
 </head>
 <body>
-    <h1>Hello world! Liz panget HAHAHHAHA EDI WOW</h1>
+    <h1>Hello world! Liz panget HAHAHHAHA EDI WOWSS</h1>
     <h1>HAHAHHAHAH HELLO</h1>
 
     <div style="background-color: green;"> 
