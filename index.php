4c14ada19bb84188355b81d85c1fdbfacce609ee
BriTheCode <https://github.com/BriTheCode>
2025-02-23 22:33:36 +0800

    adding haha sa h1

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include("database.php");
     <title>Dela Cruz</title>
 </head>
 <body>
-    <h1>Hello world! Liz panget</h1>
+    <h1>Hello world! Liz panget HAHAHHAHA EDI WOW</h1>
     <form action="" method="post">
          <label for="">firstname:</label>
          <br>
